22d900cca319f46550f15e7ad6524969f1868702
Corre <correbenjamin@hotmail.fr>
2019-05-20 17:22:56 +0200

    Héritage des Personnes

--- la-factory/src/main/java/fr/projet/lafactory/model/Formateur.java
+++ la-factory/src/main/java/fr/projet/lafactory/model/Formateur.java
@@ -1,5 +1,23 @@
 package fr.projet.lafactory.model;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Formateur")
+@AttributeOverrides({ @AttributeOverride(name = "id", column = @Column(name = "FOR_ID")),
+	@AttributeOverride(name = "nom", column = @Column(name = "FOR_NOM")),
+	@AttributeOverride(name = "prenom", column = @Column(name = "FOR_PRENOM")),
+	@AttributeOverride(name = "adresse", column = @Column(name = "FOR_ADRESSE")),
+	@AttributeOverride(name = "email", column = @Column(name = "FOR_ADRESSE")),
+	@AttributeOverride(name = "telephone", column = @Column(name = "FOR_TELEPHONE")),
+	@AttributeOverride(name = "motDePasse", column = @Column(name = "FOR_MOTDEPASSE")),
+	@AttributeOverride(name = "version", column = @Column(name = "FOR_VERSION")) })
 public class Formateur extends Personne {
 
+	
+	
 }
