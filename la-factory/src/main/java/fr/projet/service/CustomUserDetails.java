5121666a1ff2f00ef4991013df4ed5d12c05864e
marie <marie.durand.geo@gmail.com>
2019-05-20 16:59:44 +0200

    spring service (security) update 2

--- la-factory/src/main/java/fr/projet/service/CustomUserDetails.java
+++ la-factory/src/main/java/fr/projet/service/CustomUserDetails.java
@@ -37,7 +37,7 @@ public class CustomUserDetails implements UserDetails{
 
 	@Override
 	public String getUsername() {
-		return null;
+		return personne.getEmail();
 	}
 	
 	public String getEmail() {
