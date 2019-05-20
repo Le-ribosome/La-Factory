ebeb7827a8093d2c0a0082187332a6d1cd8f213c
EmmanuelleTEK <emmanuelle.ttek@hotmail.fr>
2019-05-20 16:43:39 +0200

    materiel video

---
+++ la-factory/src/main/java/fr/projet/lafactory/model/Videoprojecteur.java
@@ -0,0 +1,25 @@
+package fr.projet.lafactory.model;
+
+import java.util.Date;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="videoprojecteur")
+@AttributeOverrides({
+	@AttributeOverride(name="id", column=@Column(name="VID_ID")),
+	@AttributeOverride(name="code", column=@Column(name="VID_CODE")),
+	@AttributeOverride(name="coutJournee", column=@Column(name="VID_COUTJOURNEE")),
+)}
+
+public class Videoprojecteur extends Materiel {
+
+	private boolean estDisponible(Date dateDebut, Date dateFin) {
+		boolean estDispo = true;
+		return estDispo;
+	}
+}
