75b4de2a7836ac69f9d964dd383523f4da839e30
karenclotilde <karen.clotilde@gmail.com>
2019-05-20 16:13:38 +0200

    projet maven

---
+++ la-factory/src/main/java/fr/projet/lafactory/LaFactoryApplication.java
@@ -0,0 +1,13 @@
+package fr.projet.lafactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFactoryApplication.class, args);
+	}
+
+}
