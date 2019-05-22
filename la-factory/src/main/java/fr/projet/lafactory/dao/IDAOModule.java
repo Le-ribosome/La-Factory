b7bdb4c27461348871f812f88a579da19ff0a678
Alexis <drouinalexis@hotmail.fr>
2019-05-22 17:29:57 +0200

    ajout search module by formation

--- la-factory/src/main/java/fr/projet/lafactory/dao/IDAOModule.java
+++ la-factory/src/main/java/fr/projet/lafactory/dao/IDAOModule.java
@@ -1,10 +1,14 @@
 package fr.projet.lafactory.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import fr.projet.lafactory.model.Formation;
 import fr.projet.lafactory.model.Module;
 
 public interface IDAOModule extends JpaRepository<Module, Integer>{
 
+	public List<Module> findAllByFormation(Formation formation);
 	
 }
