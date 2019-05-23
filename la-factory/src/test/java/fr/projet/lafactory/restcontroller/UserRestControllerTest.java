765de237391ad409440e1db1051ca1d63806f2b5
Corre <correbenjamin@hotmail.fr>
2019-05-23 10:17:25 +0200

    Test UserRest (1 méthode)

--- la-factory/src/test/java/fr/projet/lafactory/restcontroller/UserRestControllerTest.java
+++ la-factory/src/test/java/fr/projet/lafactory/restcontroller/UserRestControllerTest.java
@@ -1,33 +1,48 @@
 package fr.projet.lafactory.restcontroller;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = UserRestController.class)
+import fr.projet.lafactory.LaFactoryApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = LaFactoryApplication.class)
+@WebAppConfiguration
 public class UserRestControllerTest {
-	
-	@Autowired
+
 	private MockMvc mockMvc;
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@Before
+	public void beforeTest() {
+		assertNotNull(webApplicationContext);
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 
 	@Test
 	public void testAll() throws Exception {
 
 		try {
-			mockMvc.perform(get("/all")).andExpect(status().isOk()).andExpect(view().name("all"));
+			mockMvc.perform(get("/rest/user/all")).andExpect(status().isOk())/*.andExpect(view().name("home"))*/;
 
 		} catch (Exception e) {
 			fail("Liste des personnes non validée.");
 		}
 	}
-	
+
 }
