c8881afde3bfd2442871a007bdd1f3965f0e170d
Alexis <drouinalexis@hotmail.fr>
2019-05-23 11:22:13 +0200

    Joe la bidouille
    ajout findFormationByTitre

--- la-factory/src/main/java/fr/projet/lafactory/dao/IDAOFormation.java
+++ la-factory/src/main/java/fr/projet/lafactory/dao/IDAOFormation.java
@@ -1,9 +1,12 @@
 package fr.projet.lafactory.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.projet.lafactory.model.Formation;
 
 public interface IDAOFormation extends JpaRepository<Formation, Integer> {
 
+	public Optional<Formation> findFormationByTitre(String titre);
 }
